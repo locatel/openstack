0f07e69e0c6f98d33ae3631adb1468a8decf4674
Maxime Aubry <m.aubry@hexanet.fr>
2015-05-20 14:37:27 +0200

    gestion des snapshots

---
+++ src/Volume/v2/Models/Snapshot.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace OpenStack\Volume\v2\Models;
+
+use OpenStack\Common\Resource\AbstractResource;
+use OpenStack\Common\Resource\IsListable;
+use OpenStack\Common\Resource\IsRetrievable;
+
+/**
+ * Represents a Volume v2 Volume.
+ *
+ * @property \OpenStack\Volume\v2\Api $api
+ */
+class Snapshot extends AbstractResource implements IsListable, IsRetrievable
+{
+    public $id;
+    public $name;
+    public $status;
+    public $size;
+    public $description;
+    public $links;
+
+    protected $resourceKey = 'snapshot';
+    protected $resourcesKey = 'snapshots';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function retrieve()
+    {
+        $response = $this->execute($this->api->getSnapshot(), ['id' => (string) $this->id]);
+        $this->populateFromResponse($response);
+    }
+}
